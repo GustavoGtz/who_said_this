85aac06adacaa420bbda02820ed7e5d9afe17086
Rei <gustavogtznav@gmail.com>
2025-12-06 17:11:51 -0700

    Initial project structure

---
+++ src/who/chat_operations.py
@@ -0,0 +1,5 @@
+# This file will contain 3 fundamental operations
+
+# 1. get_members_list
+# 2. get_messages_length (filters)
+# 3. get_messages_list (filters)
---
+++ src/who/whatsapp/chat_reader.py
@@ -0,0 +1,1 @@
+# This file will contain a concrete implementation of a chat_reader using Whatsapp
