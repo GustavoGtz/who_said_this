68e946c490b1623e0bb1508311e9857ba6faad78
Rei <gustavogtznav@gmail.com>
2025-12-09 18:01:58 -0700

    Upsi :P I forgot to upload this change

--- backend/who/protocols.py
+++ backend/who/protocols.py
@@ -16,23 +16,11 @@ class ChatReader(Protocol):
 class MessagesManager(Protocol):
 
     def get_samples(self,sample_size: int, sample_count: int) -> Sequence[Sequence[Message]]:
-        """
-        Return a collection of message samples.
-        Each sample contains `sample_size` messages, and there are `sample_count` samples.
-        """
         ...
 
     def generate_question(self, options_number: int,) -> Sequence[Message, list[str]]:
-        """
-        Generate a question consisting of:
-        - A reference message (the question)
-        - A list of possible answer options (one of them being the correct one)
-        """
         ...
 
     def set_messages(self, new_messages: Sequence[Message]) -> None:
-        """
-        Replace the current messages with a new sequence of messages.
-        """
         ...
     
